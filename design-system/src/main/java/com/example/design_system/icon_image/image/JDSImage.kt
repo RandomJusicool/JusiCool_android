7752cc11e4310d4fdb0606218790809ff40647b0
명훈 <s23065@gsm.hs.kr>
2024-06-17 23:50:47 +0900

    :memo: :: Add JDSImage

---
+++ design-system/src/main/java/com/example/design_system/icon_image/image/JDSImage.kt
@@ -0,0 +1,88 @@
+package com.example.design_system.icon_image.image
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.jusiCool.`design-system`.R
+
+@Composable
+fun CardsImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.cards_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun ChatFloatImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.chat_float_button_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun CloudImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.cloud_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun CommunityFloatButtonImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.community_float_button_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun GraphImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.chat_float_button_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun HeartImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.heart_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun LogoImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun SendImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.send_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun SpEEchBubbleImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.speech_bubble_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
