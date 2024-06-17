c63304bf4a707471c7671214b0425d64761e9c32
명훈 <s23065@gsm.hs.kr>
2024-06-17 20:53:55 +0900

    :sparkles: :: Create JDSColor Element

---
+++ design-system/src/main/java/com/example/design_system/theme/color/ColorTheme.kt
@@ -0,0 +1,24 @@
+package com.example.design_system.theme.color
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+    abstract val MAIN: Color
+    abstract val Black: Color
+    abstract val WHITE: Color
+
+    abstract val GRAY800: Color
+    abstract val GRAY700: Color
+    abstract val GRAY600: Color
+    abstract val GRAY500: Color
+    abstract val GRAY400: Color
+    abstract val GRAY300: Color
+    abstract val GRAY200: Color
+    abstract val GRAY100: Color
+    abstract val GRAY50: Color
+
+    abstract val GRAY2: Color
+    abstract val GRAY1: Color
+
+    abstract val ERROR: Color
+}
