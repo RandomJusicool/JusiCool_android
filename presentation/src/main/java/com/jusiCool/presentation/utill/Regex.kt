1de6fba6b3f03497259eb7907c9fd48d35b69d13
명훈 <s23065@gsm.hs.kr>
2024-06-19 00:06:04 +0900

    :sparkles: :: Regex File(Util Package)

--- presentation/src/main/java/com/jusiCool/presentation/utill/Regex.kt
+++ presentation/src/main/java/com/jusiCool/presentation/utill/Regex.kt
@@ -1,11 +1,11 @@
 package com.jusiCool.presentation.utill
 
 fun String.checkEmailRegex(): Boolean {
-    val emailRegex = "dd" // 임시 -> 후에 정규화식 작성?
+    val emailRegex = "dd"
     return this.matches(emailRegex.toRegex())
 }
 
 fun String.checkPasswordRegex(): Boolean {
-    val passwordRegex = "dd" // 임시 -> 후에 정규화식 작성?
+    val passwordRegex = "dd"
     return this.matches(passwordRegex.toRegex())
 }
