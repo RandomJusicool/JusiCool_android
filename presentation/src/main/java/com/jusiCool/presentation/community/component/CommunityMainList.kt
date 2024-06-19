708c2c02416ae40fc834aca04b5855dabf84787a
명훈 <s23065@gsm.hs.kr>
2024-06-20 08:37:05 +0900

    :lipstick: :: Create CoummityMainList(Item)

---
+++ presentation/src/main/java/com/jusiCool/presentation/community/component/CommunityMainList.kt
@@ -0,0 +1,44 @@
+package com.jusiCool.presentation.community.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.design_system.theme.JusiCoolAndroidTheme
+
+@Composable
+fun CommunityMainList(
+    modifier: Modifier = Modifier,
+    data: List<TemList> = listOf(),
+    navigateToDetailCommunity: () -> Unit
+) {
+    JusiCoolAndroidTheme { colors, _ ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = colors.GRAY50)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 8.dp
+                )
+        ) {
+            itemsIndexed(data) {_, item ->
+                CommunityMainListItem(
+                    data = item,
+                    onClick = navigateToDetailCommunity
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CommunityMainListPre() {
+    CommunityMainList{}
+}
