356fb1d7341517fa04dcaa6333981cbcda151f56
명훈 <s23065@gsm.hs.kr>
2024-06-20 17:01:01 +0900

    :memo: :: Apply Package Changed

--- presentation/src/main/java/com/jusiCool/presentation/communityList/component/CommunityMainItem.kt
+++ presentation/src/main/java/com/jusiCool/presentation/communityList/component/CommunityMainItem.kt
@@ -1,4 +1,4 @@
-package com.jusiCool.presentation.community.component
+package com.jusiCool.presentation.communityList.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +11,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
