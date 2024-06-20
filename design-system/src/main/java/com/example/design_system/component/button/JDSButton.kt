e4249af4659ad9d5a4adc0beba2a189a2fc75991
answad <mymhj122@gmail.com>
2024-06-20 22:42:30 +0900

    JDSCustomButton 추가

--- design-system/src/main/java/com/example/design_system/component/button/JDSButton.kt
+++ design-system/src/main/java/com/example/design_system/component/button/JDSButton.kt
@@ -1,12 +1,15 @@
 package com.example.design_system.component.button
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Button
@@ -14,17 +17,22 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.design_system.component.modifier.clickableSingle.clickableSingle
+import com.example.design_system.theme.JDSTypography
 import com.example.design_system.theme.JusiCoolAndroidTheme
+import com.example.design_system.theme.color.JDSColor
 
 @Composable
 fun JDSButton(
     modifier: Modifier = Modifier,
     text: String,
     state: ButtonState = ButtonState.Enable,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     JusiCoolAndroidTheme { colors, typography ->
 
@@ -49,7 +57,7 @@ fun JDSButton(
             contentPadding = PaddingValues(vertical = 16.dp),
             shape = RoundedCornerShape(12.dp),
             onClick = onClick,
-            ) {
+        ) {
             Text(
                 text = text,
                 style = typography.bodyMedium
@@ -62,7 +70,7 @@ fun JDSButton(
 fun JDSMainColorOutLineButton(
     modifier: Modifier = Modifier,
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     JusiCoolAndroidTheme { colors, typography ->
         OutlinedButton(
@@ -88,7 +96,7 @@ fun JDSMainColorOutLineButton(
 fun JDSRedColorOutLineButton(
     modifier: Modifier = Modifier,
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     JusiCoolAndroidTheme { colors, typography ->
         OutlinedButton(
@@ -110,6 +118,34 @@ fun JDSRedColorOutLineButton(
     }
 }
 
+
+@Composable
+fun JDSCustomButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    buttonColor: Color = JDSColor.MAIN,
+    textColor: Color = JDSColor.WHITE,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = buttonColor,
+                shape = RoundedCornerShape(12.dp),
+            )
+            .clickableSingle { onClick() }
+            .padding(horizontal = 32.dp, vertical = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = JDSTypography.bodyMedium,
+            color = textColor
+        )
+    }
+}
+
+
 @Preview
 @Composable
 fun JDSButtonPreview() {
@@ -125,12 +161,17 @@ fun JDSButtonPreview() {
             text = "o0뀨0o",
             modifier = Modifier.fillMaxWidth(),
             state = ButtonState.Disable
-            ) {}
+        ) {}
         JDSMainColorOutLineButton(
             text = "o0뀨0o",
             modifier = Modifier.fillMaxWidth()
         ) {}
         JDSRedColorOutLineButton(
+            text = "o0뀨0o",
+            modifier = Modifier.fillMaxWidth()
+        ) {}
+        JDSCustomButton(
+            buttonColor = JDSColor.ERROR,
             text = "o0뀨0o",
             modifier = Modifier.fillMaxWidth()
         ) {}
