c371ecd6c63b524e94972c0e62d46372921459c1
answad <mymhj122@gmail.com>
2024-06-20 19:55:47 +0900

    navgation 관련 함수 생성

---
+++ presentation/src/main/java/com/jusiCool/presentation/stockDetail/screen/StockDetailScreen.kt
@@ -0,0 +1,58 @@
+package com.jusiCool.presentation.stockDetail.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+const val stockDetail = "stockDetail"
+
+// 화면이동을 구현하는 NavController확장함수
+fun NavController.navigationToStockDetail() {
+    this.navigate(stockDetail)
+}
+
+// navHost에 화면을 등록할 수 있게 하는 확장 함수
+fun NavGraphBuilder.stockDetailRoute(
+    popUpBackStack: () -> Unit,
+    navigateToStockBuying: () -> Unit,
+    navigateToStockSell: () -> Unit,
+) {
+    composable(stockDetail) {
+        StockDetailRoute(
+            popUpBackStack = popUpBackStack,
+            navigateToStockBuying = navigateToStockBuying,
+            navigateToStockSell = navigateToStockSell,
+        )
+    }
+}
+
+// Route
+
+@Composable
+fun StockDetailRoute(
+    modifier: Modifier = Modifier,
+    popUpBackStack: () -> Unit,
+    navigateToStockBuying: () -> Unit,
+    navigateToStockSell: () -> Unit,
+) {
+    StockDetailScreen(
+        modifier = modifier,
+        popUpBackStack = popUpBackStack,
+        navigateToStockBuying = navigateToStockBuying,
+        navigateToStockSell = navigateToStockSell,
+    )
+}
+
+// Screen
+
+@Composable
+fun StockDetailScreen(
+    modifier: Modifier = Modifier,
+    popUpBackStack: () -> Unit,
+    navigateToStockBuying: () -> Unit,
+    navigateToStockSell: () -> Unit,
+) {
+
+}
