96dcacd8be70bb6a08dfabd8fc780675f8dbd709
명훈 <s23065@gsm.hs.kr>
2024-07-01 21:15:06 +0900

    :sparkles: :: Create JusiCoolApplication

---
+++ app/src/main/java/com/jusiCool/jusicool_android/JusiCoolApplication.kt
@@ -0,0 +1,8 @@
+package com.jusiCool.jusicool_android
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JusiCoolApplication: Application() {}
