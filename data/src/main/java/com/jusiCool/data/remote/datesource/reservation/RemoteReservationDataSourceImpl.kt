b8b6f41efa6ccb85b6f49158118b009fa434ff1b
명훈 <s23065@gsm.hs.kr>
2024-07-01 16:11:23 +0900

    :sparkles: :: Create RemoteReservationDataSource(Impl)

---
+++ data/src/main/java/com/jusiCool/data/remote/datesource/reservation/RemoteReservationDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.jusiCool.data.remote.datesource.reservation
+
+import com.jusiCool.data.remote.api.ReservationAPI
+import com.jusiCool.data.remote.datesource.board.RemoteBoardDataSource
+import com.jusiCool.data.remote.dto.reservation.response.GetReserveStockResponse
+import com.jusiCool.data.utill.performApiRequest
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RemoteReservationDataSourceImpl @Inject constructor(
+    private val serviceReservation: ReservationAPI
+) : RemoteReservationDataSource {
+    override suspend fun getReservationStock(): Flow<GetReserveStockResponse> =
+        performApiRequest { serviceReservation.getReservationStock() }
+}
