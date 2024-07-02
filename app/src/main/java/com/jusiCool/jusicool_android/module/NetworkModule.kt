e51e0d96f9de215af37844f6833c2e80094cff7f
명훈 <s23065@gsm.hs.kr>
2024-07-02 23:55:21 +0900

    :memo: :: Update NetworkModule To StockAPI

--- app/src/main/java/com/jusiCool/jusicool_android/module/NetworkModule.kt
+++ app/src/main/java/com/jusiCool/jusicool_android/module/NetworkModule.kt
@@ -6,6 +6,7 @@ import com.jusiCool.data.remote.api.BoardAPI
 import com.jusiCool.data.remote.api.CommentAPI
 import com.jusiCool.data.remote.api.EmailAPI
 import com.jusiCool.data.remote.api.ReservationAPI
+import com.jusiCool.data.remote.api.StockAPI
 import com.jusiCool.data.utill.AuthInterceptor
 import com.squareup.moshi.Moshi
 import dagger.Module
@@ -98,5 +99,11 @@ object NetworkModule {
     @Singleton
     fun CommunityAPI(retrofit: Retrofit): CommentAPI {
         return retrofit.create(CommentAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun StockAPI(retrofit: Retrofit): StockAPI {
+        return retrofit.create(StockAPI::class.java)
     }
 }
