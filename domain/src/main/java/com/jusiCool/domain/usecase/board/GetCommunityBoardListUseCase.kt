61a34c89597924baec67873774f22f845a5daaab
명훈 <s23065@gsm.hs.kr>
2024-07-04 11:47:27 +0900

    :memo: :: Modify Naming

---
+++ domain/src/main/java/com/jusiCool/domain/usecase/board/GetCommunityBoardListUseCase.kt
@@ -0,0 +1,12 @@
+package com.jusiCool.domain.usecase.board
+
+import com.jusiCool.domain.repository.BoardRepository
+import javax.inject.Inject
+
+class GetCommunityBoardListUseCase @Inject constructor(
+    private val repository: BoardRepository
+){
+    suspend operator fun invoke(communityId: Long) = runCatching {
+        repository.getCommunityBoardList(communityId = communityId)
+    }
+}
