e4b1edc0266e2a19dfec285ac7229641bf8967c1
명훈 <s23065@gsm.hs.kr>
2024-07-09 21:48:38 +0900

    :memo: :: Add communityData(CommunityListViewModel)

--- presentation/src/main/java/com/jusiCool/presentation/communityList/viewModel/CommunityListViewModel.kt
+++ presentation/src/main/java/com/jusiCool/presentation/communityList/viewModel/CommunityListViewModel.kt
@@ -1,15 +1,18 @@
 package com.jusiCool.presentation.communityList.viewModel
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jusiCool.domain.model.community.response.GetCommunityListResponseModel
+import com.jusiCool.domain.repository.CommunityRepository
 import com.jusiCool.domain.usecase.community.GetCommunityListUseCase
 import com.jusiCool.presentation.utill.Event
 import com.jusiCool.presentation.utill.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
@@ -17,13 +20,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CommunityListViewModel @Inject constructor(
-    private val getCommunityListUseCase: GetCommunityListUseCase
+    private val getCommunityListUseCase: GetCommunityListUseCase,
 ) : ViewModel() {
     private val _swipeRefreshLoading = MutableStateFlow(false)
     val swipeRefreshLoading = _swipeRefreshLoading.asStateFlow()
 
     private val _getCommunityListResponse = MutableStateFlow<Event<List<GetCommunityListResponseModel>>>(Event.Loading)
     val getCommunityListResponse = _getCommunityListResponse.asStateFlow()
+
+    private lateinit var _communityData : MutableState<GetCommunityListResponseModel>
+    val communityData: MutableState<GetCommunityListResponseModel>
+        get() = _communityData
 
     init {
         loadStuff()
