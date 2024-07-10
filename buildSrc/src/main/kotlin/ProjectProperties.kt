555b11bfbd1a499bafee100dc54d14de166fd580
youloveli <mymhj122@gmail.com>
2024-07-10 12:09:23 +0900

    fire :: 로그 삭제

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -7,7 +7,7 @@ object ProjectProperties {
     }
 
     object Id {
-        const val APPLICATION_ID = "com.jusiCool.jusiCool_android"
+        const val APPLICATION_ID = "com.jusiCool.JusiCoolApplication"
     }
 
     object Files {
