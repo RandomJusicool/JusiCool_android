0126ac2d6b6e8a203edbbd269c8b08aecb05b9a0
youloveli <mymhj122@gmail.com>
2024-07-10 12:12:31 +0900

    fire :: 로그 삭제

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -7,7 +7,7 @@ object ProjectProperties {
     }
 
     object Id {
-        const val APPLICATION_ID = "com.jusiCool.JusiCoolApplication"
+        const val APPLICATION_ID = "com.chobo.mindway_v2_android"
     }
 
     object Files {
