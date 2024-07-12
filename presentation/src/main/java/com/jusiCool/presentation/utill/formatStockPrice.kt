2af96ffed4ff87a199da1109cd30d64334d4f873
명훈 <s23065@gsm.hs.kr>
2024-07-12 13:41:02 +0900

    :memo: :: Add formateLongStockPrice To formatStockPrice Package

--- presentation/src/main/java/com/jusiCool/presentation/utill/formatStockPrice.kt
+++ presentation/src/main/java/com/jusiCool/presentation/utill/formatStockPrice.kt
@@ -3,3 +3,5 @@ package com.jusiCool.presentation.utill
 fun Int.formatStockPrice(): String = "%,d".format(this)
 
 fun String.formatStockPrice(): String = this.toInt().formatStockPrice()
+
+fun Long.formatLongStockPrice(): String = "%,d".format(this)
