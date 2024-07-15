bdf9cfb5b5cbcf3b4d424c3687934e43248e8458
명훈 <s23065@gsm.hs.kr>
2024-07-15 11:36:24 +0900

    :sparkles: :: Create GetReceipt(Dto and Model)

---
+++ data/src/main/java/com/jusiCool/data/remote/dto/receipt/response/GetReceipt.kt
@@ -0,0 +1,13 @@
+package com.jusiCool.data.remote.dto.receipt.response
+
+import com.jusiCool.domain.enumtype.ReceiptEnumType
+import com.jusiCool.domain.model.receipt.response.GetReceiptModel
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class GetReceipt(
+    @Json(name = "stockName") val stockName: String,
+    @Json(name = "status") val status: ReceiptEnumType,
+    @Json(name = "price") val price: Long,
+)
