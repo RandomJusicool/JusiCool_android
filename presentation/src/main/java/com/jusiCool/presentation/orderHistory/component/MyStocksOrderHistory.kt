d0dc9733b65caf356d7ef57f46da6838e007dec4
명훈 <s23065@gsm.hs.kr>
2024-07-15 19:49:09 +0900

    :memo: :: Apply Model To Component

--- presentation/src/main/java/com/jusiCool/presentation/orderHistory/component/MyStocksOrderHistory.kt
+++ presentation/src/main/java/com/jusiCool/presentation/orderHistory/component/MyStocksOrderHistory.kt
@@ -1,6 +1,5 @@
 package com.jusiCool.presentation.orderHistory.component
 
-import android.graphics.Paint.Style
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,31 +11,27 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.design_system.theme.JDSTypography
 import com.example.design_system.theme.color.JDSColor
-
-data class MyStocksOrderHistoryData(
-    val stockName: String,
-    val price: Int,
-    val isSell: Boolean,
-)
+import com.jusiCool.domain.enumtype.ReceiptEnumType
+import com.jusiCool.domain.model.receipt.response.GetReceiptModel
 
 @Composable
 fun MyStocksOrderHistory(
     modifier: Modifier = Modifier,
-    myStocksOrderHistoryData: MyStocksOrderHistoryData
+    data: GetReceiptModel
 ) {
-    val formmatedMyPrice = "%,d".format(myStocksOrderHistoryData.price)
+    val formmatedMyPrice = "%,d".format(data.price)
 
     Column(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
         Text(
-            text = myStocksOrderHistoryData.stockName,
+            text = data.stockName,
             style = JDSTypography.bodySmall,
             color = JDSColor.Black
         )
 
-        if(myStocksOrderHistoryData.isSell) {
+        if(data.status == ReceiptEnumType.BUY) {
             Text(
                 text = "${formmatedMyPrice}원 구매완료",
                 style = JDSTypography.label,
@@ -57,6 +52,6 @@ fun MyStocksOrderHistory(
 fun MyStocksOrderHistoryPreview() {
     MyStocksOrderHistory(
         modifier = Modifier.width(312.dp),
-        myStocksOrderHistoryData = MyStocksOrderHistoryData("마이크로소프트", 37250, false)
+        data = GetReceiptModel(ReceiptEnumType.BUY, "adf", 12)
     )
 }
