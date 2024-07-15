59f7603eea35b4752d60fe6b194a5c0957de9388
명훈 <s23065@gsm.hs.kr>
2024-07-15 13:20:30 +0900

    :memo: :: Apply Before Commit(Add Status)

--- data/src/main/java/com/jusiCool/data/remote/datesource/receipt/RemoteReceiptDataSource.kt
+++ data/src/main/java/com/jusiCool/data/remote/datesource/receipt/RemoteReceiptDataSource.kt
@@ -4,5 +4,5 @@ import com.jusiCool.data.remote.dto.receipt.response.GetReceipt
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteReceiptDataSource {
-    suspend fun getReceipt() : Flow<List<GetReceipt>>
+    suspend fun getReceipt(status: String) : Flow<List<GetReceipt>>
 }
--- domain/src/main/java/com/jusiCool/domain/usecase/receipt/GetReceiptUseCase.kt
+++ domain/src/main/java/com/jusiCool/domain/usecase/receipt/GetReceiptUseCase.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 class GetReceiptUseCase @Inject constructor(
     private val repository: ReceiptRepository
 ) {
-    suspend operator fun invoke() = runCatching {
-        repository.getReceipt()
+    suspend operator fun invoke(status: String) = runCatching {
+        repository.getReceipt(status = status)
     }
 }
