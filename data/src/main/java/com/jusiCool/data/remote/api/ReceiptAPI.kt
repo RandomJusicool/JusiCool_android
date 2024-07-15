7dd0b4e038036048349314d6bd3743950ef8592d
명훈 <s23065@gsm.hs.kr>
2024-07-15 13:13:39 +0900

    :memo: :: Add Status Query To ReceiptAPI

--- data/src/main/java/com/jusiCool/data/remote/api/ReceiptAPI.kt
+++ data/src/main/java/com/jusiCool/data/remote/api/ReceiptAPI.kt
@@ -2,9 +2,12 @@ package com.jusiCool.data.remote.api
 
 import com.jusiCool.data.remote.dto.receipt.response.GetReceipt
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ReceiptAPI {
 
     @GET("/api/v1/receipt")
-    suspend fun getReceipt() : List<GetReceipt>
+    suspend fun getReceipt(
+        @Query("status") status: String
+    ) : List<GetReceipt>
 }
