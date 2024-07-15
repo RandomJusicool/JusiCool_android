303e3a5f3895861a5790e20d6f2f546c03c8c330
명훈 <s23065@gsm.hs.kr>
2024-07-15 19:47:25 +0900

    :fire: :: Fire Unused Import

--- presentation/src/main/java/com/jusiCool/presentation/communityDetail/component/CommentCardList.kt
+++ presentation/src/main/java/com/jusiCool/presentation/communityDetail/component/CommentCardList.kt
@@ -1,6 +1,5 @@
 package com.jusiCool.presentation.communityDetail.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,8 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.design_system.theme.JusiCoolAndroidTheme
 import com.jusiCool.domain.model.comment.response.GetCommunityCommentResponseModel
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun CommentCardList(
